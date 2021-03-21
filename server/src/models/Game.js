e5a2ad8a91f0c289a5bdd00e516075986e819c97
vikceen <voh@live.se>
2021-03-21 18:51:05 +0100

    Forgot to export the class

--- server/src/models/Game.js
+++ server/src/models/Game.js
@@ -1,6 +1,7 @@
 
 class Game {
-    constructor(user1, user2, currentPlayer, gameState, gameOver, draw, winner) {
+    constructor(id, user1, user2, currentPlayer, gameState, gameOver, draw, winner) {
+        this.id = id;
         this.user1 = user1;
         this.user2 = user2;
         this.currentPlayer = currentPlayer;
@@ -8,6 +9,7 @@ class Game {
         this.gameOver = gameOver;
         this.draw = draw;
         this.winner = winner;
-
     }
 }
+
+module.exports = Game;
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,10 +1,10 @@
 
 class User {
-
     constructor(id, name, password) {
         this.id = id;
         this.name = name;
         this.password = password;
     }
-
 }
+
+module.exports = User;
