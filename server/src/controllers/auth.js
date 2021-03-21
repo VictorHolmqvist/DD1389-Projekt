5458ed8d369816b89b95ae2c5ed7d8222823a932
vikceen <voh@live.se>
2021-03-21 18:52:49 +0100

    Updated method calls to the db class

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,34 +1,39 @@
 const express = require('express');
+const db = require('../database.js');
 
 const router = express.Router();
 
 
 router.post('/authenticate', (req, res) => {
-    res.sendStatus(200);
-    // db.getTeacher(null, req.body.username).then((teacher) => {
-    //     if (req.body.password && req.body.password === teacher.password) {
-    //         // Update the userID of the currently active session
-    //         const authToken = uuidv4();
-    //         req.session.authToken = authToken;
-    //         console.log(`NEW TOKEN: ${authToken}`);
-    //         sessionManager.addAuthenticatedUser(authToken, teacher, req.session.socketID);
-    //         req.session.save((err) => {
-    //             if (err) {
-    //                 console.error(err);
-    //             } else {
-    //                 console.debug(`Saved authToken: ${req.session.authToken}`);
-    //                 res.status(200).json({
-    //                     username: teacher.name,
-    //                 });
-    //             }
-    //         });
-    //     } else {
-    //         res.sendStatus(401);
-    //     }
-    // }).catch((err) => {
-    //     console.error(err.message);
-    //     res.sendStatus(401);
-    // });
+    db.getUserByName(req.body.username).then((user) => {
+        if (req.body.password && req.body.password === user.password) {
+            // Update the userID of the currently active session
+            // const authToken = uuidv4();
+            // req.session.authToken = authToken;
+            // console.log(`NEW TOKEN: ${authToken}`);
+            // sessionManager.addAuthenticatedUser(authToken, teacher, req.session.socketID);
+            // req.session.save((err) => {
+            //     if (err) {
+            //         console.error(err);
+            //     } else {
+            //         console.debug(`Saved authToken: ${req.session.authToken}`);
+            //         res.status(200).json({
+            //             username: user.name,
+            //         });
+            //     }
+            // });
+
+            res.status(200).json({
+                username: user.name,
+            });
+
+        } else {
+            res.sendStatus(401);
+        }
+    }).catch((err) => {
+        console.error(err.message);
+        res.sendStatus(401);
+    });
 });
 
 router.get('/isAuthenticated', (req, res) => {
@@ -48,14 +53,13 @@ router.post('/register', (req, res) => {
     const { username } = req.body;
     const { password } = req.body;
 
-    res.sendStatus(200)
-    // db.addTeacher(username, password).then((resp) => {
-    //     console.log(`Success adding new teacher: ${resp}`);
-    //     res.sendStatus(200);
-    // }).catch((err) => {
-    //     console.error(err.message);
-    //     res.sendStatus(400);
-    // });
+    db.addUser(username, password).then((resp) => {
+        console.log(`Success adding new user: ${resp}`);
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.error(err.message);
+        res.sendStatus(400);
+    });
 });
 
 
