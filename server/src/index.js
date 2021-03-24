5bae3e1a2f1ca915d8773e71bdfd2f833ba32cb7
HannesSundin <hannessu@kth.se>
2021-03-24 10:22:23 +0100

    Revert "Formatting"
    This reverts commit a5eabe32df0875feba420cd519d68fe6f28a3752.

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const betterLogging = require('better-logging');
 
 const { Theme } = betterLogging;
 betterLogging(console, {
-  color: Theme.green,
+    color: Theme.green,
 });
 
 const path = require('path'); // helper library for resolving relative paths
@@ -22,11 +22,11 @@ const io = require('socket.io').listen(httpServer); // Creates socket.io app
 
 // Setup middleware
 app.use(betterLogging.expressMiddleware(console, {
-  ip: { show: true, color: Theme.green.base },
-  method: { show: true, color: Theme.green.base },
-  header: { show: false },
-  path: { show: true },
-  body: { show: true },
+    ip: { show: true, color: Theme.green.base },
+    method: { show: true, color: Theme.green.base },
+    header: { show: false },
+    path: { show: true },
+    body: { show: true },
 }));
 
 app.use(express.json());
@@ -34,17 +34,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Setup session
 const session = expressSession({
-  secret: 'Super secret! Shh! Do not tell anyone...',
-  resave: true,
-  saveUninitialized: true,
-  key: 'authToken',
+    secret: 'Super secret! Shh! Do not tell anyone...',
+    resave: true,
+    saveUninitialized: true,
+    key: 'authToken',
 });
 
 app.use(session);
 
 io.use(socketIOSession(session, {
-  autoSave: true,
-  saveUninitialized: true,
+    autoSave: true,
+    saveUninitialized: true,
 }));
 
 app.use(express.static(publicPath));
@@ -52,14 +52,14 @@ app.use(express.static(publicPath));
 const authController = require('./controllers/auth.js');
 const lobbyController = require('./controllers/lobby.js');
 const profileController = require('./controllers/profile.js');
-const requireAuth = require('./controllers/requireAuth.js');
+const requireAuth = require('./controllers/requireAuth.js')
 
 app.use('/api/auth', authController.router);
 app.use('/api/lobby', requireAuth, lobbyController.router);
 app.use('/api/profile', requireAuth, profileController.router);
 
 
-// Setup SocketManager
+//Setup SocketManager
 const socketManager = require('./socketManager.js');
 const sessionManager = require('./sessionManager.js');
 
@@ -67,24 +67,25 @@ socketManager.setIo(io);
 
 // Handle connected socket.io sockets
 io.on('connection', (socket) => {
-  const { authToken } = socket.handshake.session;
 
-  if (authToken
-    && sessionManager.getUser(authToken) !== null
-  ) {
-    // If the current user already logged in and then reloaded the page
-    socketManager.updateUserSocket(authToken, socket);
-  } else {
-    // The user is not authenticated. Assign a new socketId.
-    socket.handshake.session.socketID = socketManager.addUnregisteredSocket(socket);
-    socket.handshake.session.save((err) => {
-      if (err) console.error(err);
-      else console.debug(`Saved socketID: ${socket.handshake.session.socketID}`);
-    });
-  }
+    const authToken = socket.handshake.session.authToken;
+
+    if (authToken
+        && sessionManager.getUser(authToken) !== null
+    ) {
+        // If the current user already logged in and then reloaded the page
+        socketManager.updateUserSocket(authToken, socket);
+    } else {
+        //The user is not authenticated. Assign a new socketId.
+        socket.handshake.session.socketID = socketManager.addUnregisteredSocket(socket);
+        socket.handshake.session.save((err) => {
+            if (err) console.error(err);
+            else console.debug(`Saved socketID: ${socket.handshake.session.socketID}`);
+        });
+    }
 });
 
 // Start server
 httpServer.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+    console.log(`Listening on http://localhost:${port}`);
 });
